f8ff1579c76595d5779d1cd4d726efcc822fe00e
b3n10 <francisben.lleve@gmail.com>
2018-01-13 10:48:25 +0800

    OK: readfile data.json and push to new array. TODO: save new array to data.json

--- add-new-cli.js
+++ add-new-cli.js
@@ -1,6 +1,10 @@
 const readline = require('readline');
+const jsonfile = require('jsonfile')
+
 const rl = readline.createInterface(process.stdin, process.stdout, null);
+const file = 'data.json';
 const obj = {};
+const arr = [];
 const category = [
 	"Book",
 	"Chapter",
@@ -9,13 +13,22 @@ const category = [
 	"Text"
 ];
 
+const file_arr = jsonfile.readFileSync(file);
+
+let x = 0;
+while (x < file_arr.length) {
+	arr.push(file_arr[x]);
+	x++;
+}
+
 let i = 0;
 const getInput = function() {
 	rl.question(category[i] + ": ", (answer) => {
 		obj[category[i]] = answer;
 		i++;
 		if (i === 5) {
-			console.log(obj);
+			arr.push(obj);
+			console.log(arr);
 			return rl.close();
 		}
 		getInput();
@@ -26,5 +39,8 @@ getInput();
 /*
  * helpful article about looping through readline
  * https://jttan.com/2016/06/node-js-basic-command-line-interactive-loop/
- */
 
+ jsonfile.writeFile(file, obj, function (err) {
+ console.error(err)
+ })
+*/
